6e12188ff361dc483ee363a829718f6389bed9df
Taner Tun <t_tun@msn.com>
2022-02-16 10:18:55 +0200

    Updated

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import Todo from './Todo';
 
 function App() {
+  const [id, setId] = useState(0)
+  const incrementCounter = () => {
+    setId((previousValue) => previousValue + 1)
+  }
+
+  const [title, setTitle] = useState('')
+  const [todos, setTodos] = useState([])
+  const addTodo = () => {
+    console.log(title)
+    const newTodo = {
+      todoId: id,
+      title: title,
+      description: 'Ha en bra dag!'
+    }
+    setTodos([...todos, newTodo])
+    incrementCounter()
+  }
+  const deleteTodo = (deleteId) => {
+    const filteredTodos = todos.filter((todo) => todo.todoId !== deleteId)
+    setTodos(filteredTodos)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div style={{display: 'flex'}}>
+        <label>Title<input value={title} onChange={(event) => {
+          setTitle(event.target.value)
+        }} /></label>
+        <label>Description<input onChange={() => {
+          //Vad kan vi göra för att koppla denna input till description? :)
+        }} /></label>
+        <button onClick={addTodo}>ADD TODO</button>
+      </div>
+      {todos.map(({todoId, title, description}) => {
+        return <Todo key={todoId} id={todoId} title={title} description={description} onDelete={deleteTodo} />
+      })}
     </div>
   );
 }
